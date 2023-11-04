f52ac01819e3f50cc5699c48f504364651a28c53
Andrii <andre.senchenko@gmail.com>
2023-11-05 00:29:49 +0200

    Initial Commit

---
+++ Guess The Doodle/Guess_The_DoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_DoodleApp.swift
+//  Guess The Doodle
+//
+//  Created by Andrii on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_DoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
