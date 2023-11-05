cce951321a9ba7eb2324f8af77742aa5d4b7485e
Andrii <andre.senchenko@gmail.com>
2023-11-05 12:42:55 +0200

    GameOverView сustomization

--- Guess The Doodle/GameOverView.swift
+++ Guess The Doodle/GameOverView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct GameOverView: View {
+    @ObservedObject var matchManager: MatchManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Image("gameOver")
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal, 70)
+                .padding(.vertical)
+            Text("Score: \(matchManager.score)")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(Color("primaryYellow"))
+            Spacer()
+            Button {
+                // TODO: Go back to menu
+            } label: {
+                Text("Menu")
+                    .foregroundColor(Color("menuBtn"))
+                    .brightness(-0.4)
+                    .font(.largeTitle)
+                    .bold()
+            }
+            .disabled(matchManager.authenticationSate != .authenticated || matchManager.inGame)
+            .padding()
+            .padding(.horizontal, 50)
+            .background(
+                Capsule(style: .circular)
+                    .fill(Color("menuBtn"))
+            )
+            Spacer()
+        }
+        .background(
+            Image("gameOverBg")
+                .resizable()
+                .scaledToFill()
+                .scaleEffect(1.2)
+        )
+        .ignoresSafeArea()
     }
 }
 
 struct GameOverView_Previews: PreviewProvider {
     static var previews: some View {
-        GameOverView()
+        GameOverView(matchManager: MatchManager())
     }
 }
